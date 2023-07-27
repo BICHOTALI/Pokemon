b9b6e40e465ff4f4a742741594bf76f4b92b8987
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-26 20:44:45 -0400

    created schema

---
+++ models/Pokemon.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const pokemonSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pokemonImage: {
+        type: String,
+        required: true,
+        default: 'no-image'
+    },    
+    pokemonId: {
+        type: Number,
+        required: true
+    },
+    height: {
+        type: Number,
+    },
+    weight: {
+        type: Number
+    }
+    
+})
+
+module.exports = mongoose.model('Pokemon', pokemonSchema)
